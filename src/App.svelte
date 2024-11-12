ca11a78a37e14bd5aba1c26b46221dd134c85d86
Ben Bishop <ben@benbishopsb.com>
2024-11-11 22:58:16 -0500

    Add resizable UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
     import "./app.css";
-    import {Button} from "$lib/components/ui/button";
+    import * as Resizable from "$lib/components/ui/resizable";
 </script>
 
-<main>
-    <Button>Click Me</Button>
+<main class="flex-1">
+    <Resizable.PaneGroup direction="horizontal" class="min-h-screen h-full">
+        <Resizable.Pane defaultSize={45} minSize={35}>
+            <form class="space-y-8 p-10 h-full">
+            </form>
+        </Resizable.Pane>
+        <Resizable.Handle withHandle/>
+        <Resizable.Pane>Two</Resizable.Pane>
+    </Resizable.PaneGroup>
 </main>
 
 <style>
